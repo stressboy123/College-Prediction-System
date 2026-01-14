b08dd184e60896ca417236b355ada5298337ca14
userName <userEmail>
2026-01-14 15:17:24 +0800

    系统架构初始化

---
+++ src/main/java/com/gdut/CollegePredictionSystemApplication.java
@@ -0,0 +1,15 @@
+package com.gdut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liujunliang
+ * @date 2026/1/14
+ */
+@SpringBootApplication
+public class CollegePredictionSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollegePredictionSystemApplication.class, args);
+    }
+}
